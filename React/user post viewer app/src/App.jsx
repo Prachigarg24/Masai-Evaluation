7c1b26b30515d4ad7eb1d8fb447440d099aafbf6
Prachigarg24 <prachigarg858@gmail.com>
2026-01-07 16:53:27 +0530

    pushed

--- React/user post viewer app/src/App.jsx
+++ React/user post viewer app/src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import UserSelect from "./components/UserSelect";
+import PostList from "./components/PostList";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([]);
+  const [userId, setUserId] = useState("");
+  const [posts, setPosts] = useState([]);
+  const [loadingUsers, setLoadingUsers] = useState(false);
+  const [loadingPosts, setLoadingPosts] = useState(false);
+  const [sortAsc, setSortAsc] = useState(true);
+
+  
+  useEffect(() => {
+    setLoadingUsers(true);
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .finally(() => setLoadingUsers(false));
+  }, []);
+
+  
+  useEffect(() => {
+    if (!userId) return;
+
+    setLoadingPosts(true);
+
+    fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+      .then((res) => res.json())
+      .then((data) => setPosts(data))
+      .finally(() => setLoadingPosts(false));
+  }, [userId]);
+
+
+  const sortedPosts = [...posts].sort((a, b) =>
+    sortAsc
+      ? a.title.localeCompare(b.title)
+      : b.title.localeCompare(a.title)
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div style={{ padding: "20px" }}>
+      <h2>User Posts Viewer</h2>
+
+      {/* user dropdown */}
+      <UserSelect
+        users={users}
+        loading={loadingUsers}
+        setUserId={setUserId}
+      />
+
+      {/* sort button */}
+      {userId && (
+        <button onClick={() => setSortAsc(!sortAsc)}>
+          Sort {sortAsc ? "Z-A" : "A-Z"}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      )}
+
+      {/* posts list */}
+      <PostList
+        posts={sortedPosts}
+        loading={loadingPosts}
+        userId={userId}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
